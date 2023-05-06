9c8ea0e59ec24686e4b3dd970cd48286faa79d7d
Yahor Tsytsarkin <egormail88@gmail.com>
2023-05-06 12:13:27 +0300

    feat: 04-copy done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const folderCopy = path.join(__dirname, 'files-copy')
+const folder = path.join(__dirname, 'files')
+ fs.mkdir(folderCopy, { recursive: true }, () => {
+ fs.readdir(folderCopy,{withFileTypes: true}, (err, files) => {
+  for (dirent of files){
+    if (dirent.isFile()){
+const fileName = path.basename(dirent.name);
 
+fs.unlink(`${folderCopy}/${fileName}`, () => {
+  // console.log(`Delete ${fileName} from ${folderCopy}` );
+})
+    }
+  }
+ } )
+ fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+  for (const dirent of files){
+    if (dirent.isFile()){
+      const fileName = path.basename(dirent.name);
+      fs.copyFile(`${folder}/${fileName}`, 
+      `${folderCopy}/${fileName}`, () => {
+});
+    }
+  }
+});
+} );
